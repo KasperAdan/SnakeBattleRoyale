d54de5d65a1633c4a831ba3605ea9b6d6bd22562
Daphne <daphneboelhouwers@gmail.com>
2020-10-10 12:07:11 +0200

    Layout van beginscherm gewijzigd

--- SnakeBattleRoyal/SnakeBattleRoyal/Beginscherm.Designer.cs
+++ SnakeBattleRoyal/SnakeBattleRoyal/Beginscherm.Designer.cs
@@ -29,15 +29,18 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Beginscherm));
             this.namePlayer = new System.Windows.Forms.TextBox();
             this.ipHost = new System.Windows.Forms.TextBox();
             this.portHost = new System.Windows.Forms.TextBox();
             this.connectButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // namePlayer
             // 
-            this.namePlayer.Location = new System.Drawing.Point(327, 150);
+            this.namePlayer.Location = new System.Drawing.Point(335, 163);
             this.namePlayer.Name = "namePlayer";
             this.namePlayer.Size = new System.Drawing.Size(100, 20);
             this.namePlayer.TabIndex = 0;
@@ -45,7 +48,7 @@
             // 
             // ipHost
             // 
-            this.ipHost.Location = new System.Drawing.Point(327, 176);
+            this.ipHost.Location = new System.Drawing.Point(335, 189);
             this.ipHost.Name = "ipHost";
             this.ipHost.Size = new System.Drawing.Size(100, 20);
             this.ipHost.TabIndex = 1;
@@ -53,15 +56,17 @@
             // 
             // portHost
             // 
-            this.portHost.Location = new System.Drawing.Point(327, 202);
+            this.portHost.Location = new System.Drawing.Point(335, 215);
             this.portHost.Name = "portHost";
+            this.portHost.ReadOnly = true;
             this.portHost.Size = new System.Drawing.Size(100, 20);
             this.portHost.TabIndex = 2;
+            this.portHost.Text = "7777";
             this.portHost.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // connectButton
             // 
-            this.connectButton.Location = new System.Drawing.Point(340, 228);
+            this.connectButton.Location = new System.Drawing.Point(348, 241);
             this.connectButton.Name = "connectButton";
             this.connectButton.Size = new System.Drawing.Size(75, 23);
             this.connectButton.TabIndex = 3;
@@ -69,17 +74,32 @@
             this.connectButton.UseVisualStyleBackColor = true;
             this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SnakeBattleRoyal.Properties.Resources.snake_icon;
+            this.pictureBox1.Location = new System.Drawing.Point(313, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(142, 131);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
             // Beginscherm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.connectButton);
             this.Controls.Add(this.ipHost);
             this.Controls.Add(this.portHost);
             this.Controls.Add(this.namePlayer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Beginscherm";
             this.Text = "Beginscherm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -90,6 +110,7 @@
         private System.Windows.Forms.TextBox ipHost;
         private System.Windows.Forms.TextBox portHost;
         private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- SnakeBattleRoyal/SnakeBattleRoyal/Beginscherm.cs
+++ SnakeBattleRoyal/SnakeBattleRoyal/Beginscherm.cs
@@ -26,5 +26,6 @@ namespace SnakeBattleRoyal
             frm.Show();
             this.Hide();
         }
+
     }
 }
